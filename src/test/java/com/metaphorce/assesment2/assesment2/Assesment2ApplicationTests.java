d4c61b47bb252caca5cda088bb833424d1849747
Lur <luispe020505@gmail.com>
2025-07-04 18:24:28 -0600

    commit

---
+++ src/test/java/com/metaphorce/assesment2/assesment2/Assesment2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metaphorce.assesment2.assesment2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assesment2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
